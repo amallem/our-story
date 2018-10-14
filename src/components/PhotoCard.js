f6cc9dc402442fc8937e8c28411d18e29d07077e
amallem <anirudh.mallem@rutgers.edu>
2018-10-14 02:49:15 -0700

    card flip

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Flipcard from '@kennethormandy/react-flipcard'
 
 const Image = styled.img`
   width: 100%;
 `;
 
 class PhotoCard extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      flipped: false,
+    }
+  }
+
   render() {
     const { imgSrc } = this.props;
     return (
       <div>
-        <Image src={require(`../assets/${imgSrc}`)} alt={`${imgSrc}-image`} />
+        <button onClick={e => this.setState({ flipped: !this.state.flipped })}>
+          Flip
+        </button>
+        <Flipcard flipped={this.state.flipped} type="horizontal">
+          <div>
+            <Image src={require(`../assets/${imgSrc}`)} alt={`${imgSrc}-image`} />
+          </div>
+          <div><p>I am Back!!!</p></div>
+        </Flipcard>
       </div>
     );
   }
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -25,11 +25,7 @@ class Home extends Component {
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
             icon={<WorkIcon />}
           >
-            <h3 className="vertical-timeline-element-title">Art Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-            <p>
-              Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-            </p>
+            <PhotoCard imgSrc="fav.jpg" />
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
